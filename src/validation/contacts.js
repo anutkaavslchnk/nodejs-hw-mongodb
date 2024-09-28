b0d23cc2f5b852a718c4067f189bb637cf730137
anutkaavslchnk <anutochkaaaaa14@gmail.com>
2024-09-29 00:03:14 +0200

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,17 +3,21 @@ import Joi from 'joi';
 export const createContactSchema=Joi.object({
     name:Joi.string().min(3).max(20).required(),
     email:Joi.string().email().required(),
-  gender: Joi.string().valid('male', 'female', 'other').required(),
+  gender: Joi.string().valid('male', 'female', 'other'),
+  isFavourite:Joi.boolean().required(),
+  contactType:Joi.string(),
   age:Joi.string().max(3),
   onDuty: Joi.boolean(),
   phoneNumber: Joi.string().pattern(/^\+\d{10,15}$/).optional(),
 });
 
 export const updatedContactSchema=Joi.object({
-    name:Joi.string().min(3).max(20),
-    email:Joi.string().email(),
-    age:Joi.string().max(3),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  onDuty: Joi.boolean(),
-  phoneNumber: Joi.string().pattern(/^\+\d{10,15}$/).optional(),
+  name:Joi.string().min(3).max(20),
+  email:Joi.string().email(),
+gender: Joi.string().valid('male', 'female', 'other'),
+isFavourite:Joi.boolean(),
+contactType:Joi.string(),
+age:Joi.string().max(3),
+onDuty: Joi.boolean(),
+phoneNumber: Joi.string().pattern(/^\+\d{10,15}$/).optional(),
 });
